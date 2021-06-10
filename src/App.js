0586d9ecc1c4bdb5f30bf14f6ca4caf1c195f304
Arpita Saha <arpita.sahaa@gmail.com>
2021-06-10 09:41:25 -0400

    useState functionality added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 import PasswordGenerator from "./PasswordGenerator";
 import DisplayPassword from "./DisplayPassword";
+import reactDom from "react-dom";
 
 function App() {
+  const [criteria, setCriteria] = useState({
+    length: "",
+    lowerCase: false,
+    upperCase: false,
+    specialCharacters: false,
+    numeric: false,
+  });
   return (
     <div className="App">
       <h1>Password Generator</h1>
-      <PasswordGenerator/>
-      <DisplayPassword/>
+      <PasswordGenerator criteria={criteria} setCriteria={setCriteria}/>
+      <DisplayPassword />
     </div>
   );
 }
--- src/PasswordGenerator.jsx
+++ src/PasswordGenerator.jsx
@@ -1,26 +1,56 @@
-import React from 'react'
+import React from "react";
 
-function PasswordGenerator() {
-    return (
-        <div>
-            <label>
-            length of password : 
-            <input type="text"></input><br/>
-            </label>
+function PasswordGenerator({ criteria, setCriteria }) {
 
-            <input type="checkbox" name="LowerCase"></input>
-            <label for="LowerCase">LowerCase</label><br/><br/>
 
-            <input type="checkbox" name="UpperCase"></input>
-            <label for="UpperCase">UpperCase</label><br/><br/>
+  return (
+    <div>
+      <label>
+        length of password :
+        <input onChange={(e)=>setCriteria({...criteria, length: Number(e.target.value) })} type="text" value={criteria.length}></input>
+        <br />
+      </label>
 
-            <input type="checkbox" name="Special characters"></input>
-            <label for="Special characters">Special characters</label><br/><br/>
+      <input
+        onClick={()=>setCriteria({...criteria,  lowerCase: !criteria.lowerCase })}
+        type="checkbox"
+        value={criteria.lowerCase}
+        name="LowerCase"
+      ></input>
+      <label for="LowerCase">LowerCase</label>
+      <br />
+      <br />
 
-            <input type="checkbox" name="Numeric"></input>
-            <label for="Numeric">Numeric</label><br/>
-        </div>
-    )
+      <input
+        onClick={()=>setCriteria({...criteria,  upperCase: !criteria.upperCase })}
+        type="checkbox"
+        value={criteria.upperCase}
+        name="UpperCase"
+      ></input>
+      <label for="UpperCase">UpperCase</label>
+      <br />
+      <br />
+
+      <input
+        onClick={()=>setCriteria({...criteria, specialCharacters: !criteria.specialCharacters })}
+        type="checkbox"
+        value={criteria.specialCharacters}
+        name="Special characters"
+      ></input>
+      <label for="Special characters">Special characters</label>
+      <br />
+      <br />
+
+      <input
+        onClick={()=>setCriteria({...criteria,  numeric: !criteria.numeric })}
+        type="checkbox"
+        value={criteria.numeric}
+        name="Numeric"
+      ></input>
+      <label for="Numeric">Numeric</label>
+      <br />
+    </div>
+  );
 }
 
-export default PasswordGenerator
+export default PasswordGenerator;
